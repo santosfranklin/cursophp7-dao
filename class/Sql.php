fa0192e6ad02fd5dba6ccb76f8662dd6673f57a8
santosfranklin <santosfranklinsilva@gmail.com>
2019-05-26 12:57:13 -0300

    Atualização dos aruquivos

--- class/Sql.php
+++ class/Sql.php
@@ -11,20 +11,20 @@ public function __construct(){
 
 }
 
-private function setParams($statment, $parameters = array()){
+private function setParams($statement, $parameters = array()){
 
 		foreach ($parameters as $key => $value){
 
-		$this->setParam($key, $value);
+		$this->setParam($statement, $key, $value);
 
 	}
 
 
 }
 
-private function setParam($statment, $key, $value) {
+private function setParam($statement, $key, $value) {
 
-	$setParam->bindParam($key, $value);
+	$statement->bindParam($key, $value);
 }
 
 public function query($rawQuery, $params = array()){
